829083236aeb1c8fdabf99d4996b823423692683
Amad Salmon <amad.salmon@icloud.com>
2020-09-21 16:52:08 +0200

    feat: implemented the split plane

--- src/grapher/ui/Main.java
+++ src/grapher/ui/Main.java
@@ -1,9 +1,14 @@
 package grapher.ui;
 
-import java.awt.event.MouseMotionListener;
+import java.awt.Dimension;
 
 import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JSplitPane;
 import javax.swing.SwingUtilities;
+
+import grapher.fc.Function;
 
 
 public class Main extends JFrame {
@@ -11,22 +16,38 @@ public class Main extends JFrame {
 		super(title);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
-		Grapher grapher = new Grapher();		
+		
+		
+		Grapher grapher = new Grapher();	
 		for(String expression : expressions) {
 			grapher.add(expression);
 		}
+		
+		JList<Function> listScrollPane = new JList<Function>(grapher.functions);
+		
+		// Create a split pane with the two panes in it.
+		JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
+				                           listScrollPane, grapher);
+		splitPane.setOneTouchExpandable(true);
+		splitPane.setDividerLocation(150);
+
+		// Provide minimum sizes for the two components in the split pane
+		Dimension minimumSize1 = new Dimension(100, 50);
+		listScrollPane.setMinimumSize(minimumSize1);
+		Dimension minimumSize2 = new Dimension(100, 50);
+		grapher.setMinimumSize(minimumSize2);
 		
 		Interaction i = new Interaction(grapher,this);
 		grapher.addMouseListener(i);
 		grapher.addMouseMotionListener(i);
 		grapher.addMouseWheelListener(i);
 		
-		add(grapher);
+		add(splitPane);
 		pack();
 	}
 
 	public static void main(String[] argv) {
-		final String[] expressions = argv;
+		final String[] expressions = new String[] {"cos(x)","sin(x)","tan(x)", "x*x", "atan(x)"};
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() { 
 				new Main("grapher", expressions).setVisible(true); 
