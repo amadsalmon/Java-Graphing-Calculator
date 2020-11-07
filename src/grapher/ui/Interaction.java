0e691764887a5ccce8febc571397973e91440d84
Elias <elias.el@outlook.fr>
2020-11-07 22:35:45 +0100

    feat: clip method added and drawR rewritten

--- src/grapher/ui/Interaction.java
+++ src/grapher/ui/Interaction.java
@@ -8,11 +8,9 @@ import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
 import java.awt.event.MouseWheelEvent;
-import java.awt.event.MouseWheelListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -26,8 +24,10 @@ import grapher.fc.Function;
 import grapher.fc.FunctionFactory;
 
 public class Interaction
-		implements MouseListener, MouseWheelListener, MouseMotionListener, ListSelectionListener, ActionListener {
+		 extends MouseAdapter implements ListSelectionListener, ActionListener {
 
+	final static int MARGIN = 40;
+	
 	Grapher m_grapher;
 	JScrollPane m_listScrollPane;
 	ListSelectionModel m_listSelectionModel;
@@ -35,8 +35,9 @@ public class Interaction
 	JButton m_minusButton;
 	int m_x, m_y;
 	int m_button;
-	int m_state;
+	int m_state; //TODO: replace int with real state
 	Point m_start, m_end;
+	boolean m_drawR;
 
 	public Interaction(JScrollPane scrollListView, ListSelectionModel listSelectionModel, Grapher grapher,
 			JFrame frame, JButton minusButton) {
@@ -48,6 +49,7 @@ public class Interaction
 		m_button = MouseEvent.NOBUTTON;
 		m_start = new Point(0, 0);
 		m_end = new Point(0, 0);
+		m_drawR = false;
 	}
 
 	/**
@@ -72,49 +74,41 @@ public class Interaction
 			case MouseEvent.BUTTON3:
 				m_grapher.zoom(p, -5);
 				break;
-			default:;
 		}
+		m_button = MouseEvent.NOBUTTON;
 	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
-		int button = e.getButton();
+		m_button = e.getButton();
 		
-		switch (button) {
+		switch (m_button) {
 		case MouseEvent.BUTTON1:
 			m_frame.setCursor(new Cursor(Cursor.HAND_CURSOR));
 			m_button = MouseEvent.BUTTON1;
 			break;
 		case MouseEvent.BUTTON3:
 			m_frame.setCursor(new Cursor(Cursor.CROSSHAIR_CURSOR));
-			m_button = MouseEvent.BUTTON3;
+			if(m_start.x >= MARGIN && m_start.y >= MARGIN)
+				m_frame.setCursor(new Cursor(Cursor.CROSSHAIR_CURSOR));
 			m_start = new Point(e.getX(), e.getY());
 			break;
-		default:;
 		}
 	}
 
 	@Override
-	public void mouseReleased(MouseEvent e) {
+	public void mouseReleased(MouseEvent e) {		
 		m_frame.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+		
 		if (m_button == MouseEvent.BUTTON3 && m_state == MouseEvent.MOUSE_DRAGGED) {
 			m_end = new Point(e.getX(), e.getY());
-			m_grapher.zoom(m_start, m_end);
+			if (m_start.x >= MARGIN && m_start.y >= MARGIN)
+				m_grapher.zoom(m_start, m_end);
 		}
+		
 		m_button = MouseEvent.NOBUTTON;
 		m_state = MouseEvent.NOBUTTON;
-	}
-
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-
+		m_drawR = false;
 	}
 
 	@Override
@@ -122,10 +116,16 @@ public class Interaction
 		if (m_button == MouseEvent.BUTTON1)
 			m_grapher.translate(-m_x + e.getX(), -m_y + e.getY());
 
+		
 		m_x = e.getX();
 		m_y = e.getY();
 
-		m_end.setLocation(m_x, m_y);
+		if (m_button == MouseEvent.BUTTON3) {
+			m_end.setLocation(m_x, m_y);
+			if(m_start.x >= MARGIN && m_start.y >= MARGIN)
+				m_drawR = true;
+		}
+	
 		m_state = MouseEvent.MOUSE_DRAGGED;
 	}
 
@@ -134,24 +134,54 @@ public class Interaction
 		m_x = e.getX();
 		m_y = e.getY();
 	}
+	
+	public void paint(Graphics2D g, int W, int H) {
+		if(m_drawR)
+			drawR(g, W, H);
+		
+		m_grapher.repaint();
+	}
 
+
+	public void clip(Point p, int W, int H) {
+		
+		if(p.x > W + MARGIN)
+			p.x = W + MARGIN;
+		
+		if (p.x < MARGIN)
+			p.x = MARGIN;
+		
+		if(p.y > H + MARGIN)
+			p.y = H + MARGIN;
+		
+		if (p.y < MARGIN)
+			p.y = MARGIN;
+	}
+	
 	/**
 	 * Dessine un rectangle en suivant la diagonale dessinée par la souris entre le
 	 * moment où l'utilisateur presse la souris et son relâchement.
 	 */
-	public void draw(Graphics2D g) {
-		if (m_state == MouseEvent.MOUSE_DRAGGED && m_button == MouseEvent.BUTTON3) {
-			if (m_end.x - m_start.x >= 0 && m_end.y - m_start.y >= 0)
-				g.drawRect(m_start.x, m_start.y, m_end.x - m_start.x, m_end.y - m_start.y);
-			else if (m_end.x - m_start.x < 0 && m_end.y - m_start.y >= 0) {
-				g.drawRect(m_end.x, m_start.y, m_start.x - m_end.x, m_end.y - m_start.y);
-			} else if (m_end.x - m_start.x >= 0 && m_end.y - m_start.y < 0) {
-				g.drawRect(m_start.x, m_end.y, m_end.x - m_start.x, m_start.y - m_end.y);
-			} else {
-				g.drawRect(m_end.x, m_end.y, m_start.x - m_end.x, m_start.y - m_end.y);
-			}
-		}
-		m_grapher.repaint();
+	public void drawR(Graphics2D g, int W, int H) {
+		
+		Point p0 = m_start, p1 = m_end;
+
+		clip(p1, W, H);
+		
+		int width = p1.x - p0.x, height = p1.y - p0.y;
+		
+		
+		if (width >= 0 && height >= 0)
+				g.drawRect(p0.x, p0.y, width, height);
+		
+			else if (width < 0 && height >= 0) 
+				g.drawRect(p1.x, p0.y, -width, height);
+		
+		    else if (width >= 0 && height < 0) 
+				g.drawRect(p0.x, p1.y, width, -height);
+		
+			else 
+				g.drawRect(p1.x, p1.y, -width, -height);
 	}
 
 	/**
